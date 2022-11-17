5cd97cdf747512033d2b4e04558e2fdb45c797e6
Ginam <wea9677@naver.com>
2022-11-17 12:20:16 +0900

    테스트

--- app.js
+++ app.js
@@ -1,76 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const fs = require('fs');
+const port = 3000
 const bodyParser = require('body-parser');
-// const googlestrategy = require('');
-const session = require('express-session');
-const UserRouter = require('./router/userRouter');
-const path = require('path');
+// const config = require('./config/config.json');
+
+
 const app = express();
-dotenv.config();
-const passport = require('passport');
-const port = process.env.PORT
-const appleConfig = require('./config/config.json');
-const AppleAuth = require('apple-auth');
-const auth = new AppleAuth(appleConfig, path.join(__dirname, `./config${appleConfig.private_key_path}`));
-/**
- * body parser
- */
+
 app.use(express.json());
 
-
-
 app.use(bodyParser.urlencoded({extended: true}));
+const config = {
+    client_id : "com.herokuapp.applelogintest",
+    redirect_url : "https://applelogint.herokuapp.com/",
+    response_type: "code id_token",
+    state : "origin:web",
+    scope : "name email",
+    response_mode : "form_post",
+    m : 11,
+    v : "1.5.4"
 
-/**
- * Middleware
- */
+}
 
-//  app.use(session('secret'));
-app.use(session({ 
-    secret: 'SECRET',
-    resave: true,
-    saveUninitialized: true
-  }));
- app.use(passport.initialize());
- app.use(passport.session());
-
-/**
- * Router
- */
-
- app.get("/", (req, res) => {
-    console.log( Date().toString() + "GET /");
-    res.send(`<a href="${auth.loginURL()}">Sign in with Apple</a>`);
-    console.log('지나가나요')
-});
-
-app.get('/auth/apple', (req, res) =>{
-    if(req.user) return res.redirect('/');
-    fs.readFile('./view/login.html', (error, data)=>{
-        if (error) {
-            console.log(error);
-            return res.sendStatus(500);
-        }
-        res.writeHead(200, {"Content-type" : "text/html"});
-        res.end(data);
-    });
+const queryString = Object.entries(config).map(([key, value]) => `${key}=${encodeURIComponent(value)}`).join('&');
+console.log(queryString,'쿼리스트링')
+// showLog()
+app.get("/", (req, res) => {
+    
+    res.send(`<a href = https://appleid.apple.com/auth/authorize?${queryString}>Sign in with Apple</a>`);
+    console.log(queryString)
     
 });
 
-// app.get("/", (req, res) => {
-//     res.send("<a href=\"/login\">Sign in with Apple</a>");
-// });
 
 
-
-app.use('/auth', express.urlencoded({ extended: false }), UserRouter)
-
-
-
-/**
- * Server listning
- */
 const server = app.listen(port, () =>{
     console.log(port,"번 포트에서 실행")
 });
